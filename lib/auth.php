6cfff13f91308fa378cb844f7ba290faaf4f468d
Hermes <msh160130@utdallas.edu>
2016-09-26 17:11:39 -0500

    Work on Auth/Database

--- lib/auth.php
+++ lib/auth.php
@@ -1,2 +1,20 @@
 <?php
 
+class Auth {
+	
+	public function Login($username, $password) {
+		$user = self::_GetUser($username);
+	}
+	
+	public function Logout($hash) {
+		DB::Erase($hash);
+	}
+	
+	function _MakeHash($password) {
+		return password_hash($password, PASSWORD_BCRYPT, ['cost' => Config::Get("Auth.Encryption.Cost")]);
+	}
+	
+	function _VerifyHash($password, $hash) {
+		return password_verify($password, $hash);
+	}
+}
--- lib/database.php
+++ lib/database.php
@@ -1,2 +1,12 @@
 <?php
 
+class DB {
+	
+}
+
+ORM::configure(array(
+	"connection_string" => "" . Config::Get("Database.Host") . "" . Config::Get("Database.Name"),
+	"username" => Config::Get("Database.Username"),
+	"password" => Config::Get("Database.Password"),
+	"return_result_sets" => true;
+));
